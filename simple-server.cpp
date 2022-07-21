70677bd0c287ba8b90fd31a6d66cc0a55fdf70d4
Andres Rivera <andres.rivera@twc.com>
2022-07-21 00:39:24 -0700

    Server can set up listening socket and listen to incoming connnections

--- simple-server.cpp
+++ simple-server.cpp
@@ -12,10 +12,17 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string>
+#include <cstring>
 #include <stdexcept>
+#include <unistd.h>
 
 using std::cout;
 using std::endl;
+
+static const int BACKLOG = 10;
+
+
+int get_socket_and_listen(const char *port_number);
 
 /**
  * 
@@ -28,6 +35,57 @@ int main(int argc, char *argv[]){
     if(argc != 3){
         cout << "Usage: " << argv[0] << " PORT_NUMBER DIRECTORY" << endl;
         exit(1);
-    } 
+    }
+    int listen_sockfd = get_socket_and_listen(argv[1]);
+    cout << "Now listening" << endl;
+    close(listen_sockfd);
+
     return 0;
+
+}
+
+/**
+ *
+ */
+int get_socket_and_listen(const char *port_number){
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *result;
+    int listen_sockfd;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if((status = getaddrinfo(NULL, port_number, &hints, &result)) != 0){
+        perror("Failed to get address information");
+        exit(1);   
+    }
+    
+    if((listen_sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) < 0){
+        perror("Failed to create listening socket");
+        freeaddrinfo(result);
+        exit(1);
+    }
+    
+    if((status = bind(listen_sockfd, result->ai_addr, result->ai_addrlen)) < 0){
+        perror("Failed to bind socket to IP address");
+        freeaddrinfo(result);
+        exit(1);
+    }
+
+    int reuse_yes = 1;
+    freeaddrinfo(result);
+    if((status = setsockopt(listen_sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse_yes, sizeof(reuse_yes))) < 0){
+        perror("Failed to set socket options");
+        exit(1);
+    }
+    
+    if((status = listen(listen_sockfd, BACKLOG)) < 0){
+        perror("Failed to listen to incoming connections");
+        exit(1);
+    }
+    
+    return listen_sockfd;
 }
