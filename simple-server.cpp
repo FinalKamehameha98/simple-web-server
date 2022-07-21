f03ac4d7efaf5c673a39fff733af75331a719c7c
Andres Rivera <andres.rivera@twc.com>
2022-07-20 19:15:00 -0700

    Implemented checks for exact number of command line arguments

--- simple-server.cpp
+++ simple-server.cpp
@@ -8,12 +8,26 @@
  */
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <stdexcept>
 
+using std::cout;
+using std::endl;
 
-
-
+/**
+ * 
+ *
+ *
+ * @param argc Number of arguments on command line
+ * @param argv Array of arguments from command line
+ */
 int main(int argc, char *argv[]){
-    
-
+    if(argc != 3){
+        cout << "Usage: " << argv[0] << " PORT_NUMBER DIRECTORY" << endl;
+        exit(1);
+    } 
     return 0;
 }
